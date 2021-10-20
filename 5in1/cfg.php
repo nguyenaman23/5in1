b8454637ba54f6d10ab6fc2e060c7155aaae5e29
nguyenaman23 <91823952+nguyenaman23@users.noreply.github.com>
2021-10-20 16:48:23 +0700

    Add files via upload

---
+++ 5in1/cfg.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+$useragent = "Mozilla/5.0 (Linux; Android 10; Redmi 8A Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Mobile Safari/537.36";
+
+
+
+$ssid_ltc = "PHPSESSID=3nlq614servluspuunugmq8uk4";
+
+$ssid_dgb = "xxx";
+
+$ssid_trx = "PHPSESSID=icis3hcvnodlljaf83rge7lms3";
+
+$ssid_doge = "PHPSESSID=so7ucu4jh0eq3hq1t9c27009q6";
+
+$ssid_btc = "PHPSESSID=ir3qk8usdktuhu5mk77eg7use1";
